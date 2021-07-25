e3bdef0f233fdeaa081a70ec10a202e61ab10724
mikemtdev <mikemnjovu@gmail.com>
2021-07-25 16:57:20 +0200

    add: git aliases

--- src/sys-configs/aliases.sh
+++ src/sys-configs/aliases.sh
@@ -3,8 +3,9 @@
 
 # set -x
 touch ~/.bash_aliases
-# touch bash_aliases
+
 AliasesFilePath='../../data/aliases.txt'
+GitAliasesFilePath='../../data/git_aliases.txt'
 
 # Bash Aliases setup
 echo "if [ -e $HOME/.bash_aliases ]; 
@@ -13,3 +14,10 @@ echo "if [ -e $HOME/.bash_aliases ];
     fi"  >> ~/.bash_aliases
 cat $AliasesFilePath >> ~/.bash_aliases
 
+# Git Aliaes Setup
+if [ -e $HOME/.bash_aliases ]; 
+    then
+cat $GitAliasesFilePath >> ~/.gitconfig
+    else 
+        touch ~/.gitconfig
+fi
